2c01ff54a89694c3318b1198b0262f0aff979d7a
a.zavrichko <andrey@zavrichko.dev>
2021-12-27 17:41:53 +0200

    create Tank class

--- src/main/java/ru/zavrichko/Tank.java
+++ src/main/java/ru/zavrichko/Tank.java
@@ -1,5 +1,98 @@
 package ru.zavrichko;
 
 public class Tank {
+    String model;
+    int length;
+    int width;
+    int height;
+    int speed;
+    int power;
+    String armor;
+    String gunType;
+    String engineType;
+
+
+    // model
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    // length
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    // width
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    // height
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    // speed
+
+
+
+
+    // power
+
+
+    // armor
+
+
+    // gunType
+
+
+    // engineType
+
+
+
 
 }
+
+
+
+/*
+Длина корпуса, мм	5920
+Ширина, мм	3000
+Высота, мм	2405
+
+Тип брони	стальная катаная гомогенная высокой твёрдости
+Тип пушки	нарезная
+Тип двигателя	В-2
+
+Мощность двигателя, л. с.	500
+Скорость по шоссе, км/ч	54
+
+length, mm 5920
+Width, mm 3000
+Height, mm 2405
+
+Armor type rolled steel homogeneous high hardness
+Rifled gun type
+Engine type B-2
+
+Engine power, hp With. 500
+Speed on the highway, km / h 54
+
+
+*/
