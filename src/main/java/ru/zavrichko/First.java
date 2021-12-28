aff38d6842132eb61b37cb66a9e5292b4be4096d
a.zavrichko <andrey@zavrichko.dev>
2021-12-28 11:49:06 +0200

    add printInfo();

--- src/main/java/ru/zavrichko/First.java
+++ src/main/java/ru/zavrichko/First.java
@@ -2,16 +2,12 @@ package ru.zavrichko;
 
 public class First {
     public static void main(String[] args) {
-
         // Create first model
-         Tank tankFirst = new Tank("T-34", 2000, 1000, 500, 200, 500, "test3", "test2", "test1");
+        Tank tankFirst = new Tank("T-34", 2000, 1000, 500, 200, 500, "test3", "test2", "test1");
         tankFirst.printInfo();
-
 
         // Second tank
         Tank tankSecond = new Tank("Пантера", 1000, 5000, 1800, 50, 100, "test1", "test2", "test3");
-
         tankSecond.printInfo();
-
     }
 }
